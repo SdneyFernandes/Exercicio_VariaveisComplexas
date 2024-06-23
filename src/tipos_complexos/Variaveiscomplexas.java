4043ea6080117450b852e383de4a57a04096308c
SdneyFernandes <fsidney987@gmail.com>
2024-06-22 21:36:17 -0300

    initial commit

---
+++ src/tipos_complexos/Variaveiscomplexas.java
@@ -0,0 +1,14 @@
+package tipos_complexos;
+
+public class Variaveiscomplexas {
+
+    public static void main(String[] args) {
+        int valorPrimitivo = 10;
+
+        System.out.println("Valor primitivo: " + valorPrimitivo);
+
+        Integer valorWrapper = Integer.valueOf(valorPrimitivo);
+
+        System.out.println("Valor wrapper: " + valorWrapper);
+    }
+}
